d78ace86e9fbae3d0960a488b3183d7f3f7f9967
cfh <763551832@qq.com>
2018-05-03 11:48:17 +0800

    修改了welcome.blade.php中的News->新闻

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">文档</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel-news.com">新闻</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
